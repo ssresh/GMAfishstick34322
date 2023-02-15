a1d446132eeb8ad73858cf5a11b8ac846eacf49e
ssresh <125113366+ssresh@users.noreply.github.com>
2023-02-15 11:00:26 +0530

    Add files via upload

---
+++ webpack---/app/javascript/widget/views/Campaigns.vue
@@ -0,0 +1,4 @@
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('unread-message-list',{attrs:{"messages":_vm.messages}})}
+var staticRenderFns = []
+
+export { render, staticRenderFns }
---
+++ webpack---/app/javascript/widget/views/Home (1).vue
@@ -0,0 +1,4 @@
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"flex flex-1 flex-col justify-end"},[_c('div',{staticClass:"flex flex-1 overflow-auto"}),_vm._v(" "),_c('team-availability',{attrs:{"available-agents":_vm.availableAgents,"has-conversation":!!_vm.conversationSize},on:{"start-conversation":_vm.startConversation}})],1)}
+var staticRenderFns = []
+
+export { render, staticRenderFns }
